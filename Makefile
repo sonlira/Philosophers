d50da0cded875cfa98b2ea17384ff091c188d99d
Ari <aril.abcmail@gmail.com>
2025-03-21 22:02:43 +0100

    in process

--- Makefile
+++ Makefile
@@ -6,7 +6,37 @@
 #    By: abaldelo <abaldelo@student.42madrid.com    +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2025/03/12 18:42:38 by abaldelo          #+#    #+#              #
-#    Updated: 2025/03/12 18:42:39 by abaldelo         ###   ########.fr        #
+#    Updated: 2025/03/21 20:11:24 by abaldelo         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
+NAME = philo
+CC = cc
+CFLAGS = -Wall -Wextra -Werror -I include
+
+SRCDIR = src
+OBJDIR = obj
+
+SRCS = src/main.c src/validate_argv.c src/utils.c
+OBJS = $(patsubst $(SRCDIR)/%.c, $(OBJDIR)/%.o, $(SRCS))
+
+all: $(NAME)
+
+$(OBJDIR):
+	mkdir -p $(OBJDIR)
+
+$(OBJDIR)/%.o: $(SRCDIR)/%.c | $(OBJDIR)
+	$(CC) $(CFLAGS) -c $< -o $@
+
+$(NAME): $(OBJS)
+	$(CC) $(CFLAGS) $(OBJS) -o $(NAME)
+
+clean:
+	rm -f $(OBJS)
+
+fclean: clean
+	rm -f $(NAME)
+
+re: fclean all
+
+.PHONY: all clean fclean re
--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: abaldelo <abaldelo@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/12 18:10:44 by abaldelo          #+#    #+#             */
-/*   Updated: 2025/03/20 21:58:34 by abaldelo         ###   ########.fr       */
+/*   Updated: 2025/03/21 20:06:54 by abaldelo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,22 +14,13 @@
 
 int	main(int argc, char **argv)
 {
+	// t_taple			table;
+	// t_philo			philos[MAX];
+	// pthread_mutex_t	forks[MAX];
+
 	if (argc < 5 || argc > 6)
-	{
-		printf("numero de argumentos no valido: %d\n",argc);
-		return (1);
-	}
+		return (write_error("Number of arguments not valid"), 1);
 	if (!is_valid_argv(argv + 1))
-	{
-		printf("argumentos no validos\n");
 		return (1);
-	}
-	// parse_argv(&table, argc - 1, argv + 1);
-	// int i = 0;
-	// while (i < table.philos[0].num_philos)
-	// {
-	// 	printf("id: %d\n", table.philos[i].id);
-	// 	i++;
-	// }
 	return (0);
 }
